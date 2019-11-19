7462a961f5dc846cff4992d3219bb8a4962aa6a6
Riku Honkanen <riku.honkanen9595@gmail.com>
2019-11-19 18:44:51 +0200

    Rename frontpage.html to index.html and Fix typo

--- application/views.py
+++ application/views.py
@@ -2,5 +2,5 @@ from flask import Flask, request, render_template, url_for
 from application import app
 
 @app.route("/")
-def fronPage():
+def frontPage():
     return render_template('index.html')
