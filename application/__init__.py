8e838c552d6c7446422fa8829e8342656c22e60e
uberballo <kasper.paavolainen@hotmail.com>
2019-11-19 16:03:04 +0200

    Basic database

--- application/__init__.py
+++ application/__init__.py
@@ -2,3 +2,17 @@ from flask import Flask
 app = Flask(__name__)
 
 from application import views
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books.db"
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
+
+from flask_sqlalchemy import SQLAlchemy
+
+from application.books import models
+
+db.create_all()
---
+++ application/books/models.py
@@ -0,0 +1,11 @@
+from application import db
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(144), nullable=False)
+    author = db.Column(db.String(144), nullable=False)
+
+    def __init__(self, name, author):
+        self.booksName= name
+        self.author = author 
