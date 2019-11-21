8e97065fc6c22ef4e446519af0ab461cb919fd83
guotin <eero.kauhanen@helsinki.fi>
2019-11-21 16:01:19 +0200

    Add book update functionality

--- application/__init__.py
+++ application/__init__.py
@@ -7,20 +7,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 import os
 
-
-
 if os.environ.get("HEROKU"):
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 else:
-
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books.db"
     app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
-
 from application.books import models
-from application.books import views 
+from application.books import views
 
 try:
     db.create_all()
