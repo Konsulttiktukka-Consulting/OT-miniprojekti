c59c957a59d1ec0016ae4fc4e431f0eb08f3e584
guotin <eero.kauhanen@helsinki.fi>
2019-11-24 21:52:42 +0200

    Add first Behave test

--- application/tests/features/steps/steps.py
+++ application/tests/features/steps/steps.py
@@ -1,19 +1,16 @@
 from behave import *
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-driver = webdriver.Remote(
-   command_executor='http://127.0.0.1:4444/wd/hub',
-   desired_capabilities=DesiredCapabilities.HTMLUNITWITHJS)
+driver = webdriver.Chrome()
 
 @given('I am at the start page')
 def step_impl(context):
-    driver.get('https://www.google.com/')
+    driver.get('http://localhost:5000/')
 
 @when('I click the book list')
 def step_impl(context):
-    elem = driver.find_element_by_name("See all books")
+    elem = driver.find_element_by_name("all books")
     elem.click()
 
 @then('I am shown a list containing all books')
 def step_impl(context):
-    print("789")
+    assert driver.current_url == 'http://localhost:5000/books'
