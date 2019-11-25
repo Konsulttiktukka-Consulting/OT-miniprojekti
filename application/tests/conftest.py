467a181cb0237ba5afe5bd8d2702c04aa72ff1e0
Riku Honkanen <riku.honkanen9595@gmail.com>
2019-11-25 17:54:51 +0200

    Enchance UI and add Validation

--- application/tests/conftest.py
+++ application/tests/conftest.py
@@ -1,26 +1,24 @@
 #from testing.postgresql import Postgresql
 import pytest
-from application import create_app 
+from application import create_app
 from application import db as db
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 class TestConfig(object):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
     ENV = 'test'
 
 
 @pytest.yield_fixture(scope='session')
 def app():
     _app = create_app(TestConfig)
-    _app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///tests/test.db" 
-
+    _app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///tests/test.db"
 
     yield _app
-
-
 
 
 @pytest.fixture(scope='session')
@@ -28,9 +26,10 @@ def testapp(app):
     return app.test_client()
 
 
-@pytest.yield_fixture(scope='session',autouse=True)
+@pytest.yield_fixture(scope='session', autouse=True)
 def _db(app):
     db = SQLAlchemy(app=app)
+
     class Book(db.Model):
         id = db.Column(db.Integer, primary_key=True)
         name = db.Column(db.String(144), nullable=False)
