f0fd074af17a3660ea09125d72e049c5df2c993e
Erkka Rahikainen <Erkka.Rahikainen@gmail.com>
2019-12-03 19:56:50 +0200

    Fix tests

--- application/tests/video_test.py
+++ application/tests/video_test.py
@@ -1,5 +1,5 @@
 import pytest
-
+import re
 from application import db
 from application.videos.models import Video
 
@@ -7,7 +7,7 @@ from application.videos.models import Video
 def test_create(client, app):
     assert client.get("/videos/new/").status_code == 200
     client.post("/videos/", data={"title": "koiravideo",
-                                  "url": "youtube.com"})
+                                  "url": "https://www.youtube.com/watch?v=h1sRFc5FiZA"})
 
     with app.app_context():
         assert Video.query.count() == 2
