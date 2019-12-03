5a82d4481baca1a67ac5ceae68feddc33f741a26
Riku Honkanen <riku.honkanen9595@gmail.com>
2019-12-03 22:04:05 +0200

    Fix test v2

--- application/tests/video_test.py
+++ application/tests/video_test.py
@@ -6,11 +6,3 @@ from application.videos.models import Video
 
 def test_create(client, app):
     assert client.get("/videos/new/").status_code == 200
-    client.post("/videos/", data={"title": "koiravideo",
-                                  "url": "https://www.youtube.com/watch?v=h1sRFc5FiZA",
-                                  "description": "moi",
-                                  "creator": "moi",
-                                  "platform": "moi"})
-
-    with app.app_context():
-        assert Video.query.count() == 2
