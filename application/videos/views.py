ffdba7950ed574c4bb6a4fbc0f997aac9382db4e
Riku Honkanen <riku.honkanen9595@gmail.com>
2019-12-03 17:38:16 +0200

    Add video list and modify button redirects

--- application/videos/views.py
+++ application/videos/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+import flask
 
 from application import db
 from application.videos.models import Video
@@ -13,16 +14,21 @@ def video_form():
     return render_template("videos/new.html", form=form)
 
 
-@bp.route("/videos/", methods=["GET", "POST"])
+@bp.route("/videos/", methods=["POST"])
 def videos_create():
-    form = VideoForm(request.form)
-    if form.validate_on_submit():
-        newBook = Video(form.title.data, form.url.data)
-        db.session().add(newBook)
-        db.session().commit()
-        return redirect(url_for("books.books_index"))
 
-    return render_template("videos/new.html", form=form)
+    if flask.request.method == 'POST':
+        form = VideoForm(request.form)
+        if form.validate_on_submit():
+            newBook = Video(form.title.data, form.url.data)
+            db.session().add(newBook)
+            db.session().commit()
+            return redirect(url_for("books.books_index"))
+
+
+@bp.route("/videos", methods=["GET"])
+def videos_index():
+    return render_template("videos/list.html", videos=Video.query.all())
 
 
 @bp.route("/videos/<video_id>/update", methods=["GET", "POST"])
@@ -32,7 +38,7 @@ def videos_update(video_id):
     if form.validate_on_submit():
         form.populate_obj(video)
         db.session().commit()
-        return redirect(url_for("videos.videos_show",video_id = video_id))
+        return redirect(url_for("videos.videos_show", video_id=video_id))
 
     form = VideoForm(obj=video)
     return render_template("videos/update.html", video=video, form=form)
