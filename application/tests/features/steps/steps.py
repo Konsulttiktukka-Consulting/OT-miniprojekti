4cd8c2138ed0c94dda636754359935d8c26224ef
guotin <eero.kauhanen@helsinki.fi>
2019-12-07 15:40:01 +0200

    Add waits for selenium tests

--- application/tests/features/steps/steps.py
+++ application/tests/features/steps/steps.py
@@ -5,15 +5,18 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
 chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("--disable-gpu");
 driver = webdriver.Chrome(chrome_options=chrome_options)
 
 @given('I am at the start page')
 def step_impl(context):
     driver.get('http://localhost:5000/')
+    driver.implicitly_wait(10)
 
 @given('I am at the add video page')
 def step_impl(context):
     driver.get('http://localhost:5000/videos/new/')
+    driver.implicitly_wait(10)
     
 @when('I enter a valid youtube url and submit the form')
 def step_impl(context): 
@@ -21,21 +24,25 @@ def step_impl(context):
     urlform.send_keys("https://www.youtube.com/watch?v=5jKZ9KGtee0")  
     elem = driver.find_element_by_name("addButton")
     elem.click()
+    driver.implicitly_wait(10)
        
 @when('I click the video list button')
 def step_impl(context):
     elem = driver.find_element_by_name("all videos")
     elem.click()
+    driver.implicitly_wait(10)
 
 @when('I click the add video button')
 def step_impl(context):
     elem = driver.find_element_by_name("add video")
     elem.click()
+    driver.implicitly_wait(10)
        
 @then('The video is added and shown in the video list')
 def step_impl(context):
     elem = driver.find_element_by_name("all videos")
     elem.click()
+    driver.implicitly_wait(10)
     assert ('SQUISH THAT CAT' in driver.page_source)
     
 @then('I am shown a form to add a video')
@@ -53,3 +60,4 @@ def step_impl(context):
     assert ('remove video' in driver.page_source)
     elem = driver.find_element_by_name("delete")
     elem.click()
+    driver.implicitly_wait(10)
