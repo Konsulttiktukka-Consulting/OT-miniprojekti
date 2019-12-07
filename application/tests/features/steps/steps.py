51ad4f125c70081b9a2daff5c0c5983e8bb76314
guotin <eero.kauhanen@helsinki.fi>
2019-12-07 15:55:06 +0200

    Try selenium waits again

--- application/tests/features/steps/steps.py
+++ application/tests/features/steps/steps.py
@@ -11,53 +11,53 @@ driver = webdriver.Chrome(chrome_options=chrome_options)
 @given('I am at the start page')
 def step_impl(context):
     driver.get('http://localhost:5000/')
-    driver.implicitly_wait(10)
 
 @given('I am at the add video page')
 def step_impl(context):
     driver.get('http://localhost:5000/videos/new/')
-    driver.implicitly_wait(10)
     
 @when('I enter a valid youtube url and submit the form')
-def step_impl(context): 
+def step_impl(context):
+    driver.implicitly_wait(10)
     urlform = driver.find_element_by_id("url")
     urlform.send_keys("https://www.youtube.com/watch?v=5jKZ9KGtee0")  
     elem = driver.find_element_by_name("addButton")
     elem.click()
-    driver.implicitly_wait(10)
        
 @when('I click the video list button')
 def step_impl(context):
+    driver.implicitly_wait(10)
     elem = driver.find_element_by_name("all videos")
     elem.click()
-    driver.implicitly_wait(10)
 
 @when('I click the add video button')
 def step_impl(context):
+    driver.implicitly_wait(10)
     elem = driver.find_element_by_name("add video")
     elem.click()
-    driver.implicitly_wait(10)
        
 @then('The video is added and shown in the video list')
 def step_impl(context):
+    driver.implicitly_wait(10)
     elem = driver.find_element_by_name("all videos")
     elem.click()
-    driver.implicitly_wait(10)
     assert ('SQUISH THAT CAT' in driver.page_source)
     
 @then('I am shown a form to add a video')
 def step_impl(context):
+    driver.implicitly_wait(10)
     assert driver.current_url == 'http://localhost:5000/videos/new/'
     assert ('Add a new video' in driver.page_source)
     assert ('Video URL' in driver.page_source)
 
 @then('I am shown a list containing all videos')
 def step_impl(context):
+    driver.implicitly_wait(10)
     assert driver.current_url == 'http://localhost:5000/videos'
 
 @then('I can see a button to remove a video from the list')
 def step_impl(context):
+    driver.implicitly_wait(10)
     assert ('remove video' in driver.page_source)
     elem = driver.find_element_by_name("delete")
     elem.click()
-    driver.implicitly_wait(10)
