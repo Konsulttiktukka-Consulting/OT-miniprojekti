92055d85172f551d220b7e8b888b6f2abc61e501
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-04-30 23:15:41 +0300

    Refactor

--- application/videos/views.py
+++ application/videos/views.py
@@ -37,67 +37,70 @@ def video_form():
 @bp.route("/videos/", methods=["POST"])
 def videos_create():
 
-    if flask.request.method == 'POST':
-        form = VideoForm(request.form)
+    if flask.request.method != 'POST':
+        return
 
-        if form.validate_on_submit():
-            if "youtu" in form.url.data:
-                try:
-                    api_service_name = "youtube"
-                    api_version = "v3"
+    form = VideoForm(request.form)
 
-                    youtube = googleapiclient.discovery.build(
-                        api_service_name, api_version, developerKey=DEVELOPER_KEY)
+    if not form.validate_on_submit():
+        return render_template("videos/new.html", form=form)
 
-                    if "?v=" in form.url.data:
-                        video_id = re.search(
-                            r'(?<=\?v=).{11}', form.url.data)[0]
-                    else:
-                        video_id = re.search(
-                            r'(?<=\.be\/).{11}', form.url.data)[0]
+    if "youtu" in form.url.data:
+        try:
+            api_service_name = "youtube"
+            api_version = "v3"
 
-                    res = youtube.videos().list(
-                        part="snippet",
-                        id=video_id
-                    )
-                    response = res.execute()
-                    data = response["items"][0]
+            youtube = googleapiclient.discovery.build(
+                api_service_name, api_version, developerKey=DEVELOPER_KEY)
 
-                    url = data["id"]
-                    title = data["snippet"]["title"]
-                    description = data["snippet"]["description"][:150]
-                    creator = data["snippet"]["channelTitle"]
-                    platform = "youtube"
-                    new_video = Video(title, url, creator,
-                                      description, platform)
-                    db.session().add(new_video)
-                    db.session.commit()
+            if "?v=" in form.url.data:
+                video_id = re.search(
+                    r'(?<=\?v=).{11}', form.url.data)[0]
+            else:
+                video_id = re.search(
+                    r'(?<=\.be\/).{11}', form.url.data)[0]
 
-                    return redirect(url_for("videos.videos_index"))
-                except:
-                    form.url.errors = [
-                        f"Wrong url, url must be typed like 'https://www.youtube.com/watch?v=StqIbgNA35s'"]
-                    return render_template("videos/new.html", form=form)
-            elif "twitch" in form.url.data:
-                try:
-                    url = form.url.data
-                    name = re.search(r'(?<=.tv\/).*', url)[0]
-                    user = helix.user(name)
-                    title = user.display_name
-                    description = user.description
-                    creator = title
-                    platform = "twitch"
-                    new_video = Video(title, url, creator,
-                                      description, platform)
-                    db.session().add(new_video)
-                    db.session.commit()
-                    return redirect(url_for("videos.videos_index"))
-                except:
-                    form.url.errors = [
-                        f"Invalid url"]
-                    return render_template("videos/new.html", form=form)
+            res = youtube.videos().list(
+                part="snippet",
+                id=video_id
+            )
+            response = res.execute()
+            data = response["items"][0]
 
-    return render_template("videos/new.html", form=form)
+            url = data["id"]
+            title = data["snippet"]["title"]
+            description = data["snippet"]["description"][:150]
+            creator = data["snippet"]["channelTitle"]
+            platform = "youtube"
+            new_video = Video(title, url, creator,
+                              description, platform)
+            db.session().add(new_video)
+            db.session.commit()
+
+            return redirect(url_for("videos.videos_index"))
+        except:
+            form.url.errors = [
+                f"Wrong url, url must be typed like 'https://www.youtube.com/watch?v=StqIbgNA35s'"]
+            return render_template("videos/new.html", form=form)
+    if "twitch" in form.url.data:
+        try:
+            url = form.url.data
+            name = re.search(r'(?<=.tv\/).*', url)[0]
+            user = helix.user(name)
+            title = user.display_name
+            description = user.description
+            creator = title
+            platform = "twitch"
+            new_video = Video(title, url, creator,
+                              description, platform)
+            db.session().add(new_video)
+            db.session.commit()
+            return redirect(url_for("videos.videos_index"))
+        except:
+            form.url.errors = [
+                f"Invalid url"]
+            return render_template("videos/new.html", form=form)
+
 
 
 @bp.route("/videos/remove/<video_id>", methods=["POST"])
